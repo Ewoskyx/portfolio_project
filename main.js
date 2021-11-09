a94eb33b5e2e70d5ff289b52ef58fd7a3bbb714a
Ewoskyx <ewoskyx@gmail.com>
2021-11-09 12:26:36 +0300

    Update main.js for eslint

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 const toggleIcon = document.getElementById('toggleIcon');
-const toggleElement = document.getElementById("dropdownClick");
+const toggleElement = document.getElementById('dropdownClick');
 
 function toggle() {
-    
-    toggleElement.classList.toggle("hamburger");
-    if(toggleIcon.className === 'fas fa-bars') {
-        toggleIcon.className = 'fa fa-times'
-    } else {
-        toggleIcon.className = 'fas fa-bars';
-    }
+  toggleElement.classList.toggle('hamburger');
+  if (toggleIcon.className === 'fas fa-bars') {
+    toggleIcon.className = 'fa fa-times';
+  } else {
+    toggleIcon.className = 'fas fa-bars';
   }
+}
 
- 
-
-
+toggle();
