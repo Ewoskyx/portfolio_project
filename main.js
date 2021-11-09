4ad8194af12f72afd21f91b498b354323f71fc50
Ewoskyx <ewoskyx@gmail.com>
2021-11-09 10:50:26 +0300

    Add collapse menu

---
+++ main.js
@@ -0,0 +1,16 @@
+const toggleIcon = document.getElementById('toggleIcon');
+const toggleElement = document.getElementById("dropdownClick");
+
+function toggle() {
+    
+    toggleElement.classList.toggle("hamburger");
+    if(toggleIcon.className === 'fas fa-bars') {
+        toggleIcon.className = 'fa fa-times'
+    } else {
+        toggleIcon.className = 'fas fa-bars';
+    }
+  }
+
+ 
+
+
