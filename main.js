c6116c8d25284c52164e640de2615bebf9db8d58
Ewoskyx <ewoskyx@gmail.com>
2021-11-10 00:25:10 +0300

    Update the string  double quotes with singles for Eslint check

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const toggleIcon = document.getElementById("toggleIcon");
-const toggleElement = document.getElementById("dropdownClick");
-window.addEventListener("resize", resizeMobileMenu);
+const toggleIcon = document.getElementById('toggleIcon');
+const toggleElement = document.getElementById('dropdownClick');
 
 function toggle() {
-  toggleElement.classList.toggle("hamburger");
-  if (toggleIcon.className === "fas fa-bars") {
-    toggleIcon.className = "fa fa-times";
+  toggleElement.classList.toggle('hamburger');
+  if (toggleIcon.className === 'fas fa-bars') {
+    toggleIcon.className = 'fa fa-times';
   } else {
-    toggleIcon.className = "fas fa-bars";
+    toggleIcon.className = 'fas fa-bars';
   }
 }
 
 function toggleOff() {
-  if (toggleElement.className === "desktop-menu hamburger" || window.innerWidth >= 992) {
-    toggleElement.classList.remove("hamburger");
-    toggleIcon.className = "fas fa-bars";
+  if (
+    toggleElement.className === 'desktop-menu hamburger') {
+    toggleElement.classList.remove('hamburger');
+    toggleIcon.className = 'fas fa-bars';
   } else {
-    toggleElement.className = "desktop-menu";
+    toggleElement.className = 'desktop-menu';
   }
 }
 
 function resizeMobileMenu() {
   if (window.innerWidth >= 992) {
-    toggleElement.classList.remove("hamburger");
-    toggleIcon.className = "fas fa-bars";
+    toggleElement.classList.remove('hamburger');
+    toggleIcon.className = 'fas fa-bars';
   }
 }
-
+window.addEventListener('resize', resizeMobileMenu);
 toggle();
 toggleOff();
 resizeMobileMenu();
